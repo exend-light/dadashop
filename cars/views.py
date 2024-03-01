190d82f0f8ec3e31c9e2490870740729bc4d61a3
your_name <508455471@qq.com>
2024-03-02 00:40:40 +0800

    复制作参考用

--- cars/views.py
+++ cars/views.py
@@ -1,159 +1,211 @@
-import json
-
-from django.http import JsonResponse
-from goods.models import SKU
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 from django.views import View
+import json
+from goods.models import SKU
 from django_redis import get_redis_connection
-# Create your views here.
 from dadashop.decorators import require_login
+
 from django.utils.decorators import method_decorator
 
 class CartsView(View):
-    redis_conn=get_redis_connection('carts')
+    redis_conn = get_redis_connection('carts')
 
     @method_decorator(require_login)
-    def post(self,request,username):
-        data=json.loads(request.body)
-
-        sku_id=data.get('skuid')
-        count=data.get('count')
-
+    def post(self, request, username):
+        # 获取提交数据
+        data = json.loads(request.body)
+        sku_id = data.get('sku_id')
+        count = int(data.get('count'))
+        # 数据校验
+        # .....
+        # 数据校验
         try:
-            sku_object=SKU.objects.get(pk=sku_id)
+            sku_object = SKU.objects.get(pk=sku_id)
         except Exception as e:
-
-
-            context={
-                'code':40001,
-                'error':'对不起，指定的商品不存在'
-
+            context = {
+                'code': 40001,
+                'error': '对不起，指定商品不存在'
             }
-            return HttpResponse(context)
-        if sku_object.stock<sku_object.count:
-            context={
-                'code':40002,
-                'error':'对不起，指定的商品超过库存限量'
+            return JsonResponse(context)
+
+        if sku_object.stock < count:
+            context = {
+                'code': 40002,
+                'error': '商品数量超过库存限量'
             }
-
-            return HttpResponse(context)
-
-        cache_key=f'carts_{username}'
-        fieldname = sku_id
-        if not self.redis_conn.exists(cache_key):
-
-            fieldvalue=json.dumps({'number':count,'status':True})
-
-        else:
-            if not self.redis_conn.hexists(cache_key,fieldname):
-                fieldvalue=json.dumps({'number':count,'status':True})
-            else:
-                fieldvalue=self.redis_conn.hset(cache_key,fieldname)
-                jsonvalue=json.loads(fieldvalue)
-                jsonvalue['number']+=count
-                fieldvalue=json.dumps(jsonvalue)
-                self.redis_conn.hset(cache_key,fieldname,fieldvalue)
-        self.redis_conn.hset(cache_key, fieldname, fieldvalue)
-
-        context={
-            'code':200,
-            'data':{
-                'cart_count':self.redis_conn.hlen(cache_key),
-
-            },
-            'base_url':'http://127.0.0.1:8000/media/'
-
-        }
-        return JsonResponse(context)
-    @method_decorator(require_login)
-    def get(self,request,username):
-        data=[]
-        cache_key=f'carts_{username}'
-        idlist=self.redis_conn.hkey(cache_key)
-        # idlist为字节串构成的列表
-        # id__in = idlist  自动的将字节串转换为整数与id进行比较
-        sku_objects=SKU.objects.filter(id__in=idlist)
-        for sku_object in sku_objects:
-            fieldvalue=json.loads(self.redis_conn.hget(cache_key,sku_object.id))
-            obj={
-                'id':sku_object.id,
-                'name':sku_object.name,
-                'price':sku_object.price,
-                'default_image_url':sku_object.default_image_url.name,
-                'count':fieldvalue.get('number'),
-                'selected':fieldvalue.get('status'),
-                'sku_sale_attr_name':['尺寸'],
-                'sku_sale_attr_val':['15']
-            }
-            data.append(obj)
-
-        context={
-            'code':200,
-            'base_url':'http://127.0.0.1:8000/media/',
-            'data':data
-        }
-
-        return JsonResponse(context)
-
-    @method_decorator(require_login)
-    def put(self,request,username):
-        data=json.loads(request.body)
-        state=data.get('state')
-        redis_conn=get_redis_connection('carts')
+            return JsonResponse(context)
+        # 要从REDIS中获取用户的购物信息
+        ##1.缓存的键名
         cache_key = f'carts_{username}'
-        if state=='add':
-            sku_id=data.get('sku_id')
-            redis_conn.hset(cache_key,sku_id,True)
-
-        elif state=='del':
-            sku_id=data.get('sku_id')
-
-
-        elif state=='select':
-            sku_id=data.get('sku_id')
-
-
-        elif state=='unselect':
-            sku_id=data.get('sku_id')
-
-        elif state=='selectall':
-            pass
-
-        elif state == 'unselectall':
-            pass
-
-        context={
-            'code':30003,
-            'error':'不支持的请求'
+        fieldname = sku_id
+        ##2.查询KEY是否存在，以证明用户是否有过购物记录
+        if not self.redis_conn.exists(cache_key):
+            fieldvalue = json.dumps({'number': count, 'status': True})
+            self.redis_conn.hset(cache_key, fieldname, fieldvalue)
+        else:
+            if not self.redis_conn.hexists(cache_key, fieldname):
+                fieldvalue = json.dumps({'number': count, 'status': True})
+                self.redis_conn.hset(cache_key, fieldname, fieldvalue)
+            else:
+                fieldvalue = self.redis_conn.hget(cache_key, fieldname)
+                jsonvalue = json.loads(fieldvalue)
+                jsonvalue['number'] += count
+                fieldvalue = json.dumps(jsonvalue)
+                self.redis_conn.hset(cache_key, fieldname, fieldvalue)
+        '''
+        if (not self.redis_conn.exists(cache_key)) or (not self.redis_conn.hexists(cache_key,fieldname)):
+            fieldvalue = {'number':count,'status':True}
+        else:
+            fieldvalue = self.redis_conn.hget(cache_key,fieldname)
+            fieldvalue = json.loads(fieldvalue)
+            fieldvalue['number'] += count
+        fieldvalue = json.dumps(fieldvalue)
+        self.redis_conn.hset(cache_key, fieldname, fieldvalue)
+        '''
+        context = {
+            'code': 200,
+            'data': {
+                'carts_count': self.redis_conn.hlen(cache_key)
+            },
+            'base_url': 'http://127.0.0.1:8000/media/'
         }
-        return JsonResponse('ok')
+        return JsonResponse(context)
 
     @method_decorator(require_login)
-    def delete(self,request,username):
-        data=json.loads(request.body)
-        sku_id=data.get('sku_id')
-        cache_key=f'carts_{username}'
-        self.redis_conn.hdel(cache_key,sku_id)
-        context={
-            'code':200,
+    def get(self, request, username):
+        cache_key = f'carts_{username}'
+        context = {
+            'code': 200,
+            'base_url': 'http://127.0.0.1:8000/media/',
+            'data': self.generateData(username)
+        }
+        return JsonResponse(context)
+
+    @method_decorator(require_login)
+    def put(self, request, username):
+        data = json.loads(request.body)
+        state = data.get('state')
+        statelist = ['add', 'del', 'select', 'unselect', 'selectall', 'unselectall']
+        if state not in statelist:
+            context = {
+                'code': 30003,
+                'error': '不支持的请求动作'
+            }
+            return JsonResponse(context)
+        '''
+        # 商品数量增加
+        if state == 'add':
+            sku_id = data.get('sku_id')
+            fieldname = sku_id
+            cache_key = f'carts_{username}'
+            fieldvalue = json.loads(self.redis_conn.hget(cache_key,fieldname))
+            fieldvalue['number'] += 1
+            self.redis_conn.hset(cache_key,fieldname,json.dumps(fieldvalue))
+        # 商品数量减少
+        elif state == 'del':
+            sku_id = data.get('sku_id')
+            fieldname = sku_id
+            cache_key = f'carts_{username}'
+            fieldvalue = json.loads(self.redis_conn.hget(cache_key, fieldname))
+            fieldvalue['number'] -= 1
+            self.redis_conn.hset(cache_key, fieldname, json.dumps(fieldvalue))
+        # 单一选定
+        elif state == 'select':
+            sku_id = data.get('sku_id')
+            fieldname = sku_id
+            cache_key = f'carts_{username}'
+            fieldvalue = json.loads(self.redis_conn.hget(cache_key, fieldname))
+            fieldvalue['status'] = True
+            self.redis_conn.hset(cache_key, fieldname, json.dumps(fieldvalue))
+        # 取消单一选定
+        elif state == 'unselect':
+            sku_id = data.get('sku_id')
+            fieldname = sku_id
+            cache_key = f'carts_{username}'
+            fieldvalue = json.loads(self.redis_conn.hget(cache_key, fieldname))
+            fieldvalue['status'] = False
+            self.redis_conn.hset(cache_key, fieldname, json.dumps(fieldvalue))
+        # 全选
+        elif state == 'selectall':
+            cache_key = f'carts_{username}'
+            sku_id_lists = self.redis_conn.hkeys(cache_key)
+            for sku_id in sku_id_lists:
+                fieldvalue = json.loads(self.redis_conn.hget(cache_key,sku_id))
+                fieldvalue['status'] = True
+                self.redis_conn.hset(cache_key,sku_id,json.dumps(fieldvalue))
+        # 取消全选
+        elif state == 'unselectall':
+            cache_key = f'carts_{username}'
+            sku_id_lists = self.redis_conn.hkeys(cache_key)
+            for sku_id in sku_id_lists:
+                fieldvalue = json.loads(self.redis_conn.hget(cache_key, sku_id))
+                fieldvalue['status'] = False
+                self.redis_conn.hset(cache_key, sku_id, json.dumps(fieldvalue))
+        '''
+        # 商品数量增加和减少
+        if state == 'add' or state == 'del':
+            sku_id = data.get('sku_id')
+            fieldname = sku_id
+            cache_key = f'carts_{username}'
+            fieldvalue = json.loads(self.redis_conn.hget(cache_key, fieldname))
+            fieldvalue['number'] += 1 if state == 'add' else -1
+            self.redis_conn.hset(cache_key, fieldname, json.dumps(fieldvalue))
+            # 单一选定 和 取消单一选定
+        elif state == 'select' or state == 'unselect':
+            sku_id = data.get('sku_id')
+            fieldname = sku_id
+            cache_key = f'carts_{username}'
+            fieldvalue = json.loads(self.redis_conn.hget(cache_key, fieldname))
+            fieldvalue['status'] = True if state == 'select' else False
+            self.redis_conn.hset(cache_key, fieldname, json.dumps(fieldvalue))
+        # 全选 和 取消全选
+        elif state == 'selectall' or state == 'unselectall':
+            cache_key = f'carts_{username}'
+            sku_id_lists = self.redis_conn.hkeys(cache_key)
+            for sku_id in sku_id_lists:
+                fieldvalue = json.loads(self.redis_conn.hget(cache_key, sku_id))
+                fieldvalue['status'] = True if state == 'selectall' else False
+                self.redis_conn.hset(cache_key, sku_id, json.dumps(fieldvalue))
+        context = {
+            'code': 200,
+            'base_url': 'http://127.0.0.1:8000/media/',
+            'data': self.generateData(username)
+        }
+        return JsonResponse(context)
+
+    @method_decorator(require_login)
+    def delete(self, request, username):
+        data = json.loads(request.body)
+        sku_id = data.get('sku_id')
+        cache_key = f'carts_{username}'
+        self.redis_conn.hdel(cache_key, sku_id)
+        context = {
+            'code': 200,
             'data':
                 {
-                    'carts_count':self.redis_conn.hlen(cache_key)
+                    'carts_count': self.redis_conn.hlen(cache_key)
                 },
-            'base_url':'http://127.0.0.1:8000/media/'
+            'base_url': 'http://127.0.0.1:8000/media/'
         }
-        
         return JsonResponse(context)
 
-
-
-
-
-
-
-
-
-
-
-
-
+    def generateData(self, username):
+        data = []
+        cache_key = f'carts_{username}'
+        idlist = self.redis_conn.hkeys(cache_key)
+        sku_objects = SKU.objects.filter(id__in=idlist)
+        for sku_object in sku_objects:
+            fieldvalue = json.loads(self.redis_conn.hget(cache_key, sku_object.pk))
+            obj = {
+                'id': sku_object.id,
+                'name': sku_object.name,
+                'price': sku_object.price,
+                'default_image_url': sku_object.default_image_url.name,
+                'count': fieldvalue.get('number'),
+                'selected': fieldvalue.get('status'),
+                "sku_sale_attr_name": ['尺寸'],
+                "sku_sale_attr_val": ['15']
+            }
+            data.append(obj)
+        return data
--- goods/views.py
+++ goods/views.py
@@ -1,102 +1,135 @@
-import json
-
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse
 from django.http import JsonResponse
-from .models import Catalog
-from  django.db.models import F
-from .models import SKU,SPU
-from django.core.paginator import Paginator
+from django.db.models import F
+import json
 # Create your views here.
 
+from .models import Catalog
+from .models import SKU, SPU
+from .models import SPUSaleAttr, SaleAttrValue
+from django.core.paginator import Paginator
+
+
 def index(request):
-    data=[]
-    catalogs=Catalog.objects.all()
-    for catalog_objects in catalogs:
-        sku_objects=SKU.objects.values('name','caption','price',skuid=F('id'),image=F('default_image_url')).filter(spu__in=catalog_objects.spu_set.all(),is_launched=True)[0:3]
+    data = []
+    catalogs = Catalog.objects.all()
+    # 遍历所有的分类
+    for catalog_object in catalogs:
+        # 获取当前分类下的3个正常上架的商品
+        sku_objects = SKU.objects.values('name', 'caption', 'price', skuid=F('id'),
+                                         image=F('default_image_url')).filter(spu__in=catalog_object.spu_set.all(),
+                                                                              is_launched=True)[:3]
         data.append({
-            'catalog_id':catalog_objects.id,
-            'catalog_name':catalog_objects.name,
-            'sku':list(sku_objects)
+            'catalog_id': catalog_object.id,
+            'catalog_name': catalog_object.name,
+            'sku': list(sku_objects)
         })
-
-    print(data)
-    context={
-        'code':200,
-        'base_url':"http://127.0.0.1:8000/media/",
-        'data':data
-
+    context = {
+        "code": 200,
+        "base_url": "http://127.0.0.1:8000/media/",
+        "data": data
     }
     return JsonResponse(context)
 
 
-def catalogs(request,id):
+def catalogs(request, id):
     try:
-        catalog_object=Catalog.objects.get(pk=id)
-
-    except  Exception as e:
-        context={
-            'code':20001,
-            'error':'对不起，商品不存在'
-        }
-        return JsonResponse(context)
-
-    sku_objects=SKU.objects.values('name','price',skuid=F('id'),image=F('default_image_url')).filter(spu__in=catalog_object.spu_set.all(),is_launched=True).order_by('id')
-    print(sku_objects)
-
-    pagesize=1
-    pagiator=Paginator(sku_objects,pagesize)
-
-    page=request.GET.get('page')
-    print(page)
-
-    page_data=pagiator.get_page(page)
-
-    context={
-        'code':200,
-        'data':list(page_data.object_list),
-        'paginator':{
-            'pagesize':pagesize,
-            'total':sku_objects.count()
-        },
-        'base_url':'http://127.0.0.1:8000/media/'
-
-    }
-
-    return JsonResponse(context)
-
-def detail(request,id):
-    try:
-        sku_object=SKU.objects.get(pk=id,is_launched=True)
-        print(sku_object)
+        catalog_object = Catalog.objects.get(pk=id)
     except Exception as e:
-        context={
-            'code':200,
-            'error':'对不起，指定的商品不存在'
-
+        context = {
+            'code': 20001,
+            'error': '对不起，商品分类不存在'
         }
         return JsonResponse(context)
+    # 获取当前这个分类下的正常上架所有商品
+    sku_objects = SKU.objects.values('name', 'price', skuid=F('id'), image=F('default_image_url')).filter(
+        spu__in=catalog_object.spu_set.all(), is_launched=True).order_by('id')
+    # 每页显示的记录数
+    pagesize = 1
+    # 分页操作
+    pagiator = Paginator(sku_objects, pagesize)
+    # 取地址栏传递的页码
+    page = request.GET.get('page')
+    # 获取该页中的数据
+    page_data = pagiator.get_page(page)
 
-    catalog_id=sku_object.spu.catalog.id
-    catalog_name=sku_object.spu.catalog.name
-    name=sku_object.name
-    caption=sku_object.caption
-    price=sku_object.price
+    context = {
+        "code": 200,
+        # 获取当前页的数据
+        "data": list(page_data.object_list),
+        "paginator": {
+            # 每页显示的记录数
+            "pagesize": pagesize,
+            # 总记录数
+            "total": sku_objects.count()
+        },
+        "base_url": "http://127.0.0.1:8000/media/"
+    }
+    return JsonResponse(context)
 
-    image=sku_object.default_image_url.name
-    spu_id=sku_object.spu.id
-    # spu_id = sku_object.spu_id
 
-    sku_sale_attr_id=sku_object.sale_attr_value.all()
+def detail(request, id):
+    try:
+        sku_object = SKU.objects.get(pk=id, is_launched=True)
+    except Exception as e:
+        context = {
+            'code': 20002,
+            'error': '对不起，指定商品不存在'
+        }
+        return JsonResponse(context)
+    # ////////////////////////////////////////////////////
+    # ***************************
+    catalog_id = sku_object.spu.catalog.id
+    catalog_name = sku_object.spu.catalog.name
+    # ***************************
+    # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    name = sku_object.name
+    caption = sku_object.caption
+    price = sku_object.price
+    # ImageField返回django.db.models.fields.files.ImageFieldFile
+    image = sku_object.default_image_url.name
+    # 正向关系
+    spu_id = sku_object.spu.id
+    # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    # sku_sale_attrs1 = sku_object.spu.spusaleattr_set.all()
+    # sku_sale_attrs2 = SPUSaleAttr.objects.filter(spu_id = spu_id)
+    # print(sku_sale_attrs1.query)
+    # print(sku_sale_attrs2.query)
+    sku_sale_attr_id = sku_object.spu.spusaleattr_set.values_list('id', flat=True)
+    sku_sale_attr_names = sku_object.spu.spusaleattr_set.values_list('name', flat=True)
+    # print(sku_sale_attr_id)
+    # print(sku_sale_attr_names)
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    sku_all_sale_attr_vals_id={id:'abc' for id in list(sku_sale_attr_id)}
+    sku_sale_attr_val = SaleAttrValue.objects.values('id', 'name', 'spu_sale_attr_id').filter(
+        spu_sale_attr_id__in=list(sku_sale_attr_id))
 
+    # print(sku_sale_attr_val)
+    sku_sale_attr_val_id = sku_sale_attr_val.values_list('id', flat=True)
+    sku_sale_attr_val_names = sku_sale_attr_val.values_list('name', flat=True)
 
-    context= {
+    # print(sku_sale_attr_val_id)
+    # print(sku_sale_attr_val_names)
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    sku_all_sale_attr_vals_id = {id: [item.get('id') for item in list(sku_sale_attr_val.filter(spu_sale_attr_id=id))]
+                                 for id in list(sku_sale_attr_id)}
+    sku_all_sale_attr_vals_name = {
+        id: [item.get('name') for item in list(sku_sale_attr_val.filter(spu_sale_attr_id=id))] for id in
+        list(sku_sale_attr_id)}
+
+    print(sku_all_sale_attr_vals_id)
+    print(sku_all_sale_attr_vals_name)
+    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    context = {
         "code": 200,
         "data": {
             # 类1:类别id 类别name
-            "catalog_id":catalog_id,
+            "catalog_id": catalog_id,
             "catalog_name": catalog_name,
 
             # 类2：SKU
@@ -106,98 +139,83 @@ def detail(request,id):
             "image": image,
             "spu": spu_id,
 
-
-
             # 类3：详情图片
             "detail_image": "v2-1.jpg",
 
+            # 类4：SPU属性名称
+            "sku_sale_attr_id": list(sku_sale_attr_id),
+            "sku_sale_attr_names": list(sku_sale_attr_names),
 
+            # 类5：SPU属性值
+            "sku_sale_attr_val_id": list(sku_sale_attr_val_id),
+            "sku_sale_attr_val_names": list(sku_sale_attr_val_names),
 
-            # 类4：销售属性(SPU的属性名称)
-            "sku_sale_attr_id": [7, 8],
-            "sku_sale_attr_names": ["尺寸", "颜色"],
-
-
-
-            # 类5：销售属性值（SPU属性值）
-            "sku_sale_attr_val_id": [11, 12, 13],
-            "sku_sale_attr_val_names": ["18寸", "19寸", "蓝色"],
-
-
-
-            # 销售属性和销售属性值的对应关系
-            "sku_all_sale_attr_vals_id": {
-                "7": [11, 12],
-                "8": [13]
-            },
-            "sku_all_sale_attr_vals_name": {
-                "7": ["18寸", "19寸"],
-                "8": ["蓝色"]
-            },
+            # SPU属性名称和属性值的对应关系 -- id
+            "sku_all_sale_attr_vals_id": sku_all_sale_attr_vals_id,
+            # SPU属性名称和属性值的对应关系 -- name
+            "sku_all_sale_attr_vals_name": sku_all_sale_attr_vals_name,
 
             # 类6和类7：规格属性名和规格属性值
             "spec": {
                 "批次": "2000",
                 "数量": "2000",
                 "年份": "2000"
-            }
+            },
+            # 商品的图片列表
+            "images": ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
         },
         "base_url": "http://127.0.0.1:8000/media/"
     }
-
+    # ////////////////////////////////////////////////////
     return JsonResponse(context)
-
 
 
 def sku(request):
-
-    data=json.loads(request.body)
-
-    spuid=data.get('spuid')
-    spu_object=SPU.objects.get(pk=spuid)
-
-    sku_objects=spu_object.sku_set.all()
-
+    # {'1': 1, '2': 4,'6':3, 'spuid': 1}
+    # { SPU属性名称ID:SPU属性值ID }
+    data = json.loads(request.body)
+    spuid = data.get('spuid')
+    # 获取SPU对象
+    spu_object = SPU.objects.get(id=spuid)
+    # 获取到了当前这个SPU下所有的商品
+    sku_objects = spu_object.sku_set.all()
+    # 现在要遍历获取到的属性值ID，而spuid原来存在于data中
     data.pop('spuid')
-    # 根据sale_attr_value=val，一层层的过滤
-    for key,val in data.items():
-
-        sku_objects=sku_objects.filter(sale_attr_value=val)
-
-    context={
-        'code':200,
-        'data':sku_objects
-
-    }
-
-
-    return HttpResponse(context)
-
-def search(request):
-    q=request.POST.get('q')
-    print(len(q))
-    if len(q)==0:
-        context={
-            'code':30001,
-            'msg':'对不起，关键字不能为空'
+    for key, val in data.items():
+        sku_objects = sku_objects.filter(sale_attr_value=val)
+    if sku_objects:
+        context = {
+            'code': 200,
+            'data': sku_objects.first().pk
         }
-
-        return  JsonResponse(context)
-    sku_objects=SKU.objects.filter(name__contains=q).values('name','price')
-
-    context={
-        'code':200,
-        'baseurl':'http://127.0.0.1:8000/media/',
-        'pagenator':{
-            'total':20,
-            'pagesize':5
-
-        },
-        'data':list(sku_objects)
-
+        return JsonResponse(context)
+    context = {
+        'code': 30001,
+        'error': '对不起，商品不存在'
     }
     return JsonResponse(context)
 
-    
 
-
+def search(request):
+    q = request.POST.get('q')
+    if len(q) == 0:
+        context = {
+            'code': 30001,
+            'msg': '对不起，关键字不能为空'
+        }
+        return JsonResponse(context)
+    sku_objects = SKU.objects.filter(name__contains=q).values('name','price',skuid=F('id'),image=F('default_image_url'))
+    pagesize = 1
+    paginator = Paginator(sku_objects,pagesize)
+    page = request.GET.get('page',1)
+    page_data = paginator.get_page(page)
+    context = {
+        'code': 200,
+        "baseurl": "http://127.0.0.1:8000/media/",
+        'paginator': {
+            'total': sku_objects.count(),
+            'pagesize': pagesize
+        },
+        'data': list(page_data.object_list)
+    }
+    return JsonResponse(context)
